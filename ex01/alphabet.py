8a5d1f1b8ed3885b2eb92d57ed7179c305a0c625
ohi <c0b21025dc@edu.teu.ac.jp>
2022-09-27 17:24:49 +0900

    アルファベット　未完成

---
+++ ex01/alphabet.py
@@ -0,0 +1,17 @@
+from random import randint
+from random import choice
+a = 10 #対象文字数
+b = 2  #欠損文字数
+c = []
+a_ls = [] #対象文字
+b_ls = [] #欠損文字
+def syutudai(a,b):
+    for i in range(a):
+        a_ls.append(randint(65,90))
+    for i in range(b):
+        c.append(a_ls.pop(choice(a)))
+        b_ls.append(c)
+            
+syutudai(a,b)
+print(a_ls)
+print(b_ls)
